29001ceee466a4c1c1df53c0c7ac30216ead74d3
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-11-23 12:08:11 +0000

    conversion of datatype

---
+++ .devcontainer/01-basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score="74fsd"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//74 =>numbertype=>74
+//74fgh =>numbertype=>NaN
+//true=>1 ;false=>0
+
+let valueOfNumber=Number(score)
+let valueOfNumber1= String(score)
+
+console.log(typeof valueOfNumber);
+console.log(typeof valueOfNumber1);
+
+console.log(valueOfNumber);
+console.log(valueOfNumber1);
+
+let score1=null
+let numberNull=Number (score1);
+console.log(numberNull);
+
+
+let score2=undefined
+let numberUndefined= Number(score2);
+console.log(numberUndefined);
+
+let score3=true
+let numberBoolean=Number(score3)
+console.log(numberBoolean);
+
+let score4=false
+let numberBoolean1= Number(score4)
+console.log(numberBoolean1);
+
+let isLoggedIn=1 //" "(false), "arpita"(true), 0(false)
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
