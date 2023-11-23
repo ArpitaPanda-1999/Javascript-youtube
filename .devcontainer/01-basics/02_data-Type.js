51ead221193abef3bec5af13cf3ed3653e42741c
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-11-23 07:22:21 +0000

    datatype

---
+++ .devcontainer/01-basics/02_data-Type.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all js code as newer version
+
+//alert( 3+3 ) //we are using node.js not browser
+
+//console.log(3+3) console.log("arpita") //give you error //not readable code 
+//if we use a ; then it will run but not appropreate to write
+
+//primitive datatype
+//number => 2 to power 53
+//bigint => really big numbers
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefine => not assigned value
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "arpita");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
