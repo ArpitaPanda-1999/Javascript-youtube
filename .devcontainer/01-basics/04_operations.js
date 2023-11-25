043e35ec03d6ee1041109b9751a2f795be5f34ff
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-11-25 13:14:06 +0000

    datatype summary

---
+++ .devcontainer/01-basics/04_operations.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*let value=3
+let negValue= -value
+console.log(negValue);
+
+let str1="Hello"
+let str2=" Arpita"
+let str3= str1+str2
+console.log(str3);
+
+console.log("1" + 3);
+console.log("1" + "3");
+console.log(1 + "3");
+console.log("1" + 3 + 3);
+console.log(1 + 3 + "3");
+console.log(+true);
+console.log(+"");
+console.log(!'js');
+*/
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
