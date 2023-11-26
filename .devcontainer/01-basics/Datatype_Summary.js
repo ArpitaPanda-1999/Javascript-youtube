a0dbb421eaaf4bfcb84df416a72e55c76528334d
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-11-26 15:12:59 +0000

    change in datatype file

--- .devcontainer/01-basics/Datatype_Summary.js
+++ .devcontainer/01-basics/Datatype_Summary.js
@@ -7,31 +7,52 @@
 
 //const isLoggedIn=false
 //const outsideTemp=null
-let userEmail;
-console.log(userEmail);
+//let userEmail;
+//console.log(userEmail);
+//const id=Symbol('435436')
+//const anotherId=Symbol('435436')
+//console.log(id===anotherId);
 
-const id=Symbol('435436')
-const anotherId=Symbol('435436')
-console.log(id===anotherId);
-
-const bigInt=4657687980980n
-console.log(bigInt);
+//const bigInt=4657687980980n
+//console.log(bigInt);
 
 
 //Reference (non-primitive)
 
 //Array, function, Object
 
-const hero=["shaktiman", "naagraj", "doga"]
-console.log(hero);
+//const hero=["shaktiman", "naagraj", "doga"]
+//console.log(hero);
 
-let userDetail={
-    name:"Arpita",
-    age:24
-}
-console.log(userDetail);
+//let userDetail={
+ //   name:"Arpita",
+   // age:24
+//}
+//console.log(userDetail);
 
-const myFunction=function () {
-    console.log("hello world");
+//const myFunction=function () {
+//    console.log("hello world");
+//}
+//console.log(typeof myFunction);
+
+
+//stack(saves-- primitive) and heap(saves--non-primitive) memory
+
+let myYoutubeChannel="ArpitaYoutube Channels"
+
+let anotherChannel=myYoutubeChannel
+anotherChannel="twistIn life"
+
+console.log(myYoutubeChannel);
+console.log(anotherChannel);
+
+let userOne={
+    emailId:"arpita@gmail.com",
+    upi:"arpita@ybl"
 }
-console.log(typeof myFunction);
+let userTwo=userOne
+
+userTwo.emailId="twistlife@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
