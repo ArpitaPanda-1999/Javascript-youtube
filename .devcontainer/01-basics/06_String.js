a889737cfa4433b5d35a4ca92bc559113a4ca27b
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-11-27 13:13:39 +0000

    strings

---
+++ .devcontainer/01-basics/06_String.js
@@ -0,0 +1,36 @@
+const name="Arpita"
+const repoCount=20
+
+//console.log(name+ repoCount+ " value");
+
+//console.log(`hello my name is ${name} and my repo count number is ${repoCount}`);
+
+const gameName= new String('Arpitapanda    software developer')
+
+console.log(gameName.split(' '));
+
+//console.log(gameName);
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('a'));
+
+//const newString=gameName.substring(0,4)//can't use negative value,if u use it will starts from 0th index
+//console.log(newString);
+
+//const anotherString=gameName.slice(-4,3)//can use negative value 
+//console.log(anotherString);
+
+//const newStringOne="      Arpita       "
+//console.log(newStringOne);
+
+//const newStringtwo=newStringOne.trim();
+//console.log(newStringtwo);
+
+//const url="https://Arpita.com//arpita%20panda"
+//console.log(url.replace("%20","_"));
+
+//console.log(url.includes("Arpita"));
