afd9d4bd72bc8b8891bef1e8533adea219ec41a6
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-11-30 17:57:48 +0000

    String test progressing

---
+++ .devcontainer/01-basics/String-test.js
@@ -0,0 +1,13 @@
+let stringTest = new String('Arpita')
+//console.log(stringTest.anchor('panda'));
+//console.log(stringTest.charAt(2));
+//console.log(stringTest.big());
+//console.log(stringTest.blink());
+//console.log(stringTest.bold());
+//console.log(stringTest.at(3));
+//console.log(stringTest.charCodeAt(3));
+//console.log(stringTest.codePointAt(2));
+//console.log(stringTest.concat(' panda'));
+//console.log(stringTest.constructor());
+//console.log(stringTest.endsWith(1,6));
+//console.log(stringTest.fixed());
