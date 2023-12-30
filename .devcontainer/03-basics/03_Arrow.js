d5522370a99e4ebb46cead13389b85f073fd16fe
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2023-12-30 05:46:14 +0000

    Arrow Function

---
+++ .devcontainer/03-basics/03_Arrow.js
@@ -0,0 +1,38 @@
+const user = {
+    username: 'Arpita', //this function work on object only
+    price: 999,
+    
+    welcomeMessage: function () {
+        console.log(`${this.username} welcome to website`);
+        console.log(this);
+    }
+}
+//user.welcomeMessage()
+
+//user.username = 'Sonu'
+//user.welcomeMessage()
+console.log(this);
+
+function chai() {
+    let username = 'Arpita'// this function is not working on functions
+    //console.log(this.username);
+    //console.log(this);
+}
+chai()
+
+const chai1 = function () {
+    let username = 'Arpita'
+    //console.log(this.username);
+}
+chai1()
+
+const chai2 = () => {
+    let username = 'Arpita'
+    //console.log(this.username);
+}
+chai2()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3));
