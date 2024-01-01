d1572b1d5da37072f9649ed7248dc10f3d6fe8b3
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2024-01-01 05:32:20 +0000

    Switch case

---
+++ .devcontainer/04-Control-flow/Switch.js
@@ -0,0 +1,31 @@
+//switch (key) {
+//    case value:
+        
+//        break;
+
+//    default:
+//        break;
+//}
+
+const month = 3  //if the value is in string ,simply write in case "any string"
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('Febuary');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    case 5:
+        console.log('May');
+        break;
+
+    default:
+        console.log('defult case match');
+        break;
+}
