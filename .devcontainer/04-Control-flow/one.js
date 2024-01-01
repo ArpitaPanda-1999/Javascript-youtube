35d634ba5e7d90ae57896da960d637760633cbfe
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2024-01-01 05:13:03 +0000

    Control Flow

--- .devcontainer/04-Control-flow/one.js
+++ .devcontainer/04-Control-flow/one.js
@@ -1,4 +1,4 @@
-//if
+ //if
 const isUserLoggedIn = true
 const temperature = 41
 
@@ -50,14 +50,27 @@ const balance = 1000
 //if( balance > 500 ) console.log('test'), console.log('test2');// this will work but not a good practice
 
 if (balance < 500) {
-    console.log('less than 500');
+    //console.log('less than 500');
 }
 else if (balance < 750) {
-    console.log('less than 750');
+    //console.log('less than 750');
 }
 else if(balance < 900){
-    console.log('less than 900');
+    //console.log('less than 900');
 }
 else{
-    console.log('less than 1200');
+    //console.log('less than 1200');
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log('Allow to buy Course');
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log('User Logged in');
 }
