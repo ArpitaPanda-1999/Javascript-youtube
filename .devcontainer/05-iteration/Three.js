48652feb28b1e12e72a9afb7451cf35d30703354
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2024-01-10 16:59:35 +0000

    for-of and map

--- .devcontainer/05-iteration/Three.js
+++ .devcontainer/05-iteration/Three.js
@@ -35,46 +35,6 @@ for (const [key, value] of map) {
     //console.log(`${key} :- ${value}`);  
 } 
 
-//object iteration
-//const myObject = { //using object 'for of"
-    //'game1' : 'NFS',
-    //'game2' : 'Spiderman',
-   // game3 : 'NFS',
-    //game4 : 'Spiderman'
-//}
-//not going to iterate because objects don't iterate like this , for that we have different method
-//for (const [key, value] of myObject) {
-    //console.log(`${key} :- ${value}`);
-//}
-
-//object "for in" (print value)
-const myObject = {
-    js : "javascript",
-    cpp : "c++",
-    rb : "ruby",
-    swift : "swift by Apple" 
-}
-for (const key in myObject) {
-    //console.log(key);
-}
-
-for (const key in myObject) {
-    //console.log(myObject[key]);
-    //console.log(`${key} shortcut is for : ${myObject[key]}`);
-}
-
-
-//Array "for in" (print keys {started from 0} ,give numbers)
-const programming = ["js", "rb", "py", "java", "cpp"]
-
-for (const key in programming) {
-    //console.log(`programming languages are : ${key}`);
-}
-
-for (const key in programming) {
-    //  console.log(`programming languages are : ${programming[key]}`);
-}
-
 const map1 = new Map()//not iteratable
 map1.set('IN', "India")
 map1.set('USA', "United state of america")
