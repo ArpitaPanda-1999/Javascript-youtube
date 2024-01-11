3be763bf1f620d55f1e0be9332e94523db5722fc
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2024-01-11 05:07:14 +0000

    filter

--- .devcontainer/05-iteration/Six.js
+++ .devcontainer/05-iteration/Six.js
@@ -24,3 +24,27 @@ myNums.forEach((num) => {
 })
 
 //console.log(newNums);
+
+
+
+const books = [
+    {title: "Book one", genre: "Fiction", publish: 1981, edition: 2004},
+    {title: "Book two", genre: "Non-Fiction", publish: 1999, edition: 2006},
+    {title: "Book three", genre: "Science", publish: 2000, edition: 2007},
+    {title: "Book four", genre: "Fiction", publish: 1987, edition: 2009},
+    {title: "Book five", genre: "Non-Fiction", publish: 1997, edition: 2011},
+    {title: "Book six", genre: "Fiction", publish: 1988, edition: 2012},
+    {title: "Book seven", genre: "Science", publish: 2001, edition: 2010},
+    {title: "Book eight", genre: "Fiction", publish: 2012, edition: 2022},
+    {title: "Book nine", genre: "Non-Fiction", publish: 2013, edition: 2023},
+    {title: "Book ten", genre: "Fiction", publish: 1983, edition: 2021}
+];
+
+//const userBooks = books.filter( (bk) => bk.genre === "Science");
+//const userBooks = books.filter( (bk) => bk.publish >= 2000);
+//const userBooks = books.filter( (bk) => (bk.publish >= 2000));
+//const userBooks = books.filter( (bk) => {return bk.publish >= 2000});
+const userBooks = books.filter( (bk) => {
+    return bk.publish >=2000 && bk.genre === "Science";
+})
+console.log(userBooks);
