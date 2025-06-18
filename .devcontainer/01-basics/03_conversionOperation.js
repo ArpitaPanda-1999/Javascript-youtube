65b26ba21bef635ba17cb604de2bd475f2564503
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2025-06-18 06:05:43 +0000

    small change

--- .devcontainer/01-basics/03_conversionOperation.js
+++ .devcontainer/01-basics/03_conversionOperation.js
@@ -10,30 +10,39 @@ let score="74fsd"
 let valueOfNumber=Number(score)
 let valueOfNumber1= String(score)
 
-console.log(typeof valueOfNumber);
-console.log(typeof valueOfNumber1);
+console.log(typeof valueOfNumber);//number
+console.log(typeof valueOfNumber1);//string
 
-console.log(valueOfNumber);
-console.log(valueOfNumber1);
+console.log(valueOfNumber);//NaN
+console.log(valueOfNumber1);//74fsd
 
 let score1=null
 let numberNull=Number (score1);
-console.log(numberNull);
+console.log(numberNull);//0
 
 
 let score2=undefined
 let numberUndefined= Number(score2);
-console.log(numberUndefined);
+console.log(numberUndefined);//NaN
 
 let score3=true
 let numberBoolean=Number(score3)
-console.log(numberBoolean);
+console.log(numberBoolean);//1
 
 let score4=false
 let numberBoolean1= Number(score4)
-console.log(numberBoolean1);
+console.log(numberBoolean1);//0
 
 let isLoggedIn=1 //" "(false), "arpita"(true), 0(false)
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);//true
 
+let isLoggedIn1= "";
+let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1);//false
+
+let isLoggedIn2= "AP";
+let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2);//true
+
+//node .devcontainer/01-basics/03_conversionOperation.js 
