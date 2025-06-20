0d4d73212d309bb6f1b6797b418828e51025b273
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2025-06-20 05:48:42 +0000

    small change

--- .devcontainer/02-basics/01_Array.js
+++ .devcontainer/02-basics/01_Array.js
@@ -5,33 +5,33 @@ const myHeros = ["shaktiman", "naagraj", "doga"]
 //console.log(myHeros);
 
 const myArray2 = new Array(3, 5, 6, 3, 9)
-//console.log(myArray2);
+// console.log(myArray2[1]);
 
 //Array Methods
 
-//myArray.push(8);
-//myArray.pop();
+//myArray.push(8);//add a number
+//myArray.pop();//last value remove
 //myArray.unshift(9)//time consuming method because if we add somthing in arrays 1st index then we have to shift all other indexes too..it is time consuming and difficult to perform
-//myArray.shift()
+//myArray.shift();//remove first index number
 //console.log(myArray);
 
-//console.log(myArray.includes(5));
-//console.log(myArray.indexOf(3));
+//console.log(myArray.includes(5));//give result in boolean result
+
+//console.log(myArray.indexOf(3));//give result in boolean result
 
 const newArray = myArray.join()
 //console.log(myArray);
-//console.log(newArray);
-//console.log(typeof newArray); 
+//console.log(newArray); 0,1,2,3,4,5 (after conversion to string)
+//console.log(typeof newArray); //string 
 
-//console.log("A", myArray);
-
-const myNew1 = myArray.slice(0,3)
-//console.log(myNew1);
-//console.log("B", myArray);
+// console.log("A", myArray);
+const myNew1 = myArray.slice(1,3)//
+// console.log(myNew1);
+// console.log("B", myArray);
 
 const myNew2 = myArray.splice(0,3)
-//console.log(myNew2);
-//console.log("C", myArray);
+// console.log(myNew2);
+// console.log("C", myArray);
 
 //---------------------------------------------------------------
 
