b651d73583e4fda437d53973c34a17873de1228f
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2025-06-25 05:27:22 +0000

    small changes

--- .devcontainer/02-basics/04_Objects.js
+++ .devcontainer/02-basics/04_Objects.js
@@ -7,6 +7,9 @@ tinderApp1.isLoggedIn = false
 
 //console.log(tinderApp);
 //console.log(tinderApp1);
+//console.log(tinderApp1.hasOwnProperty('isLoggedIn'));
+//console.log(tinderApp1.hasOwnProperty('isLogged'));
+
 
 const regularUser = {
     email:"arpita@gmail.com",
@@ -17,13 +20,13 @@ const regularUser = {
         }
     }
 }
-//console.log(regularUser.fullname.userName.Lastname);
+//console.log(regularUser.fullname?.userName.Lastname); //"?" will safe the value to be wrong , like if there is no value persent then it will safe u from getting error;
 
 const obj1 = {1:"a", 2:"b"}
 const obj2 = {3:"c", 4:"d"}
 const obj4 = {5:'a', 6:'y'}
 //const obj3 = {obj1, obj2}
-//const obj3 = Object.assign({}, obj1, obj2)
+//const obj3 = Object.assign({}, obj1, obj2);// " {} " this is a good practice otherwise it will give u the same result
 const obj3 = {...obj1, ...obj2, ...obj4}
 //console.log(obj3);
 
@@ -31,8 +34,39 @@ const obj3 = {...obj1, ...obj2, ...obj4}
 //console.log(Object.keys(tinderApp1));
 //console.log(Object.values(tinderApp1));
 //console.log (Object.entries(tinderApp1));
-//console.log(tinderApp1.hasOwnProperty('isLoggedIn'));
-//console.log(tinderApp1.hasOwnProperty('isLogged'));
+
+
+//Array Object
+const user = [
+    {
+        id: 1,
+        name: "Arpita",
+        position: "eldest"
+    },
+    {
+        id: 2,
+        name: "Ankita",
+        position: "elder"
+    },
+    {
+        id: 3,
+        name: "Amisha",
+        position: "younger"
+    },
+    {
+        id: 4,
+        name: "Rameswar",
+        position: "youngest"
+    },
+]
+
+// console.log(user[2].name);
+// console.log(Object.keys(user));// this is a array so this is giving address
+
+// console.log(Object.keys(regularUser)); //this is giving the actual keys 
+// console.log(Object.values(regularUser)); //this will give the values stored inthe keys 
+
+
 
 const course = {
     coursename : "js in hindi",
@@ -41,4 +75,4 @@ const course = {
 }
 
 const {courseInstructor:instructor} = course //object destructure
-console.log(instructor);
+// console.log(instructor);
